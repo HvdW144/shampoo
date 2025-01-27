f8351f4590e27ea19fb9d140e19eb94999fa8b04
HvdW <117818214+HvdW144@users.noreply.github.com>
2025-01-27 17:02:00 +0100

    started on logger

---
+++ src/helpers/logger.py
@@ -0,0 +1,19 @@
+﻿import logging
+
+
+class Logger:
+    def __init__(self, level=logging.INFO, silent=True):
+        self.logger = logging.getLogger("Shampoo")
+        self.logger.setLevel(level)
+        self.silent = silent
+
+        if not self.silent:
+            console = logging.StreamHandler()
+            console.setLevel(level)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            console.setFormatter(formatter)
+            self.logger.addHandler(console)
+
+    def log(self, message, level=logging.INFO):
+        if not self.silent:
+            self.logger.log(level, message)

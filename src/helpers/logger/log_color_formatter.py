8f98d72461f45b47bb710ff8c0c9855c03ee52e4
HvdW <117818214+HvdW144@users.noreply.github.com>
2025-01-31 15:59:01 +0100

    updated logger

---
+++ src/helpers/logger/log_color_formatter.py
@@ -0,0 +1,18 @@
+﻿import logging
+
+
+class LogColorFormatter(logging.Formatter):
+    COLORS = {
+        'DEBUG': '\033[94m',  # Blue
+        'INFO': '\033[92m',  # Green
+        'WARNING': '\033[93m',  # Yellow
+        'ERROR': '\033[91m',  # Red
+        'CRITICAL': '\033[95m',  # Magenta
+        'RESET': '\033[0m'  # Reset color
+    }
+
+    def format(self, record):
+        log_color = self.COLORS.get(record.levelname, self.COLORS['RESET'])
+        reset_color = self.COLORS['RESET']
+        record.msg = f"{log_color}{record.msg}{reset_color}"
+        return super().format(record)
--- src/helpers/logger/logger.py
+++ src/helpers/logger/logger.py
@@ -1,8 +1,10 @@
 ﻿import logging
 
+from src.helpers.logger.log_color_formatter import LogColorFormatter
+
 
 class Logger:
-    def __init__(self, level=logging.INFO, silent=True):
+    def __init__(self, level=logging.DEBUG, silent=True):
         self.logger = logging.getLogger("Shampoo")
         self.logger.setLevel(level)
         self.silent = silent
@@ -10,10 +12,13 @@ class Logger:
         if not self.silent:
             console = logging.StreamHandler()
             console.setLevel(level)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            formatter = LogColorFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             console.setFormatter(formatter)
             self.logger.addHandler(console)
 
     def log(self, message, level=logging.INFO):
         if not self.silent:
             self.logger.log(level, message)
+
+    def getLogger(self):
+        return self.logger
